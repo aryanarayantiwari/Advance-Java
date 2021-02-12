7b398855e432d43991fc4e022fb9e25acbe964e5
Arya Narayan Tiwari <aryanarayantiwari@gmail.com>
2021-02-12 20:42:18 +0530

    Update First.java

--- First.java
+++ First.java
@@ -2,7 +2,10 @@ import java.awt.*;
 class First {
 public static void main(String[] args) {
 	Frame f1=new Frame();
+	Frame f2=new Frame("Frame 2");
 	f1.setSize(500,500);
 	f1.setVisible(true);
+	f2.setSize(400,400);
+	f2.setVisible(true);
 }
 }
