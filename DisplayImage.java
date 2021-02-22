3882240211d5806f56557eb7af0fc36fb9497daa
Arya Narayan Tiwari <aryanarayantiwari@gmail.com>
2021-02-22 23:45:09 +0530

    displayed an image using getDefaultToolkit().getImage method  in awt

---
+++ DisplayImage.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+import java.awt.event.*;
+class DisplayImage extends Frame{
+	DisplayImage(){
+	//frame closing
+	addWindowListener(new WindowAdapter(){
+    public void windowClosing(WindowEvent e){
+    System.exit(0);
+    } 
+    });
+	}	
+	public void paint(Graphics g){
+		//loading image
+		Image img = Toolkit.getDefaultToolkit().getImage("abc.jpg");
+		//displaying image
+		g.drawImage(img,50,60,this);
+	}
+public static void main(String[] args){
+DisplayImage d=new DisplayImage();
+d.setTitle("Display Image");
+d.setVisible(true);
+d.setSize(400,450);
+}
+}
